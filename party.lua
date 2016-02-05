089a8db33c96760cc10341035aefc59d8aba6c09
Dan Dumont <ddumont@gmail.com>
2016-02-05 02:39:32 -0500

    Forgot to account for id and index in mask and base

--- party.lua
+++ party.lua
@@ -24,12 +24,11 @@ function party.process(self, id, size, packet)
       local buffstr = '';
       local buffs = {};
       for buff = 0, 32, 1 do
-        local mask = bitpack.unpackBitsBE(packet, 4 + (48 * player), buff * 2, 2);
-        local base = struct.unpack('B', packet, 4 + 1 + (48 * player) + 8 + buff);
+        local mask = bitpack.unpackBitsBE(packet, 4 + 4 + 4 + (48 * player), buff * 2, 2);
+        local base = struct.unpack('B', packet, 4 + 1  + 4 + 4 + 8 + (48 * player) + 8 + buff);
         buffs[buff] = (256 * mask) + base;
         buffstr = buffstr .. ' ' .. buffs[buff];
       end
-      struct.unpack('B', packet, 4 + 1 + (48 * player) + 8);
       print('player ' .. player .. ': ' .. buffstr);
     end
   end
